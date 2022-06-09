bae4262c8c8e5078d98cfffb3f882e402ec510d5
Tim <tim.a.kovacs@gmail.com>
2022-06-08 21:37:42 -0400

    ch5 meadowlark.js updated

--- 05-jest/meadowlark.js
+++ 05-jest/meadowlark.js
@@ -1,4 +1,20 @@
+const express = require('express')
+
+const expressHandlebars = require('express-handlebars')
+
 const handlers = require('./lib/handlers')
+
+const app = express()
+
+// configure handlebars view engine
+app.engine('handlebars', expressHandlebars.engine({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', handlers.home)
 
@@ -9,3 +25,12 @@ app.use(handlers.notFound)
 
 // custom 500 page
 app.use(handlers.serverError)
+
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port}` +
+      '; press Ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
+}
